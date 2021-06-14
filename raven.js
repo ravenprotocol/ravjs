efc176cf0c60314db2d4db0c1eae52cf0f2d14e6
Kailash <ahikailash1@gmail.com>
2021-06-14 16:06:13 +0530

    Bugfix: where function name corrected

--- raven.js
+++ raven.js
@@ -871,7 +871,7 @@
                     let params = payload.params;
                     if ('condition' in params) {
                         let condition = params.condition;
-                        let result = a.where(condition, b);
+                        let result = a.where(condition, b).arraySync();
                         emit_result(payload, result);
                     } else {
                         emit_error(payload, {message: "Parameter 'condition' is missing" });
