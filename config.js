c4abcd628d39a328833af86cf9ea865d5efbea8d
Kailash <ahikailash1@gmail.com>
2022-04-25 15:36:38 +0530

    Ravsock host and port changed

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-const RAVSOCK_HOST = "localhost";
-const RAVSOCK_PORT = "9999";
+const RAVSOCK_HOST = location.host;
 const CLIENT_TYPE = "client";
-const GET_GRAPHS_URL = 'http://' + RAVSOCK_HOST + ':' + RAVSOCK_PORT + '/graph/get/all/';
-const SOCKET_SERVER_URL = 'ws://' + RAVSOCK_HOST + ':' + RAVSOCK_PORT + '/' + CLIENT_TYPE;
+const GET_GRAPHS_URL = 'http://' + RAVSOCK_HOST + '/graph/get/all/';
+const SOCKET_SERVER_URL = 'ws://' + RAVSOCK_HOST + '/' + CLIENT_TYPE;
 const RECONNECTION_ATTEMPTS = 5;
 const RECONNECTION = true;
