904c03c9750f07a30284c8bc5476c1a6c33a170f
Kamlesh Uikey <tokamleshuikey@gmail.com>
2022-11-29 23:31:11 +0530

    deleted unnecesssary file

--- install_chromium.sh
+++
@@ -1,5 +0,0 @@
-apt-get update
-apt-get install software-properties-common
-add-apt-repository ppa:canonical-chromium-builds/stage
-apt-get update
-apt install chromium -y
--- install_deps.sh
+++
@@ -1,3 +0,0 @@
-apt-get update
-apt-get install python3-dev python-dev python3-pip -y
-pip3 install -r requirements.txt
